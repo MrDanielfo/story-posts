e1b573f5c55434876b1d385f242451653856ba8a
Josué Daniel Flores <mr.danielfo@gmail.com>
2018-12-31 21:13:21 -0600

    handle auth

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
+const passport = require('passport'); 
 
 const app = express();
+// Llamar rutas
+const auth = require('./routes/auth');
 
+// Passport
+
+require('./config/passport')(passport); 
 
 app.get('/', (req, res) => {
     res.send('It Works'); 
 })
+
+// Requerir rutas
+app.use('/auth', auth); 
+
 
 const PORT = process.env.PORT || 5000; 
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://danielfo:danielfo89@ds013212.mlab.com:13212/storybooks-dev',
+    googleClientID: '270343017762-civlco9246l2dkrfuujm044lvf1jncvn.apps.googleusercontent.com',
+    googleClientSecret: 'ByNC7xEEyJDghdMaf6GlHpLA'
+}
